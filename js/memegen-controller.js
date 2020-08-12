2064029318d634b8551cfa70835ea40340e9dd60
dimaVais <dimavais300@gmail.com>
2020-08-12 18:23:34 +0300

    First commit

---
+++ js/memegen-controller.js
@@ -0,0 +1,63 @@
+'use strict';
+
+var gCanvas;
+var gCtx;
+var gIsImg = false;
+
+
+function init() {
+    gCanvas = document.getElementById('meme-canvas');
+    gCtx = gCanvas.getContext('2d');
+    renderGallery();
+}
+
+function onSelectImge(imgId = 2) {
+    setSelectrdImg(imgId);
+    gIsImg = false;
+    drawSelectedImage();
+}
+
+function drawSelectedImage() {
+    var selectedImg = getSelectedImg();
+    const img = new Image();
+    img.src = selectedImg.url;
+    if (!gIsImg) {
+        img.onload = () => {
+            gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height);
+            gIsImg = true;
+        }
+    } else {
+        gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height);
+    }
+}
+
+function onUpdateLine(text, x, y) {
+    drawSelectedImage();
+    updateLine(0, text, '60px', 'center', 'white');;
+    drawText(x, y);
+}
+
+
+function drawText(x, y) {
+    let line = getLineByIdx();
+    gCtx.lineWidth = '2';
+    gCtx.strokeStyle = 'black';
+    gCtx.fillStyle = line.color;
+    gCtx.font = line.size + ' impact';
+    gCtx.textAlign = line.align;
+    gCtx.fillText(line.txt, x, y);
+    gCtx.strokeText(line.txt, x, y);
+}
+
+function clearCanvas() {
+    gCtx.clearRect(0, 0, gCanvas.width, gCanvas.height);
+}
+
+function renderGallery(){
+    const gallery = getGallery();
+    var htmlImgs= gallery.map(item =>{
+        return `<img class="gallery-img" src="${item.url}" onclick="onSelectImge(${item.id})">`;
+    });
+    console.log(htmlImgs);
+    document.querySelector('.gallery-container').innerHTML = htmlImgs.join('');
+}
